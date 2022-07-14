05056282b50405845b2af8b7e857cb0a4a42d598
yasirse <yasir.arfatse@gmail.com>
2022-07-14 22:39:59 +0500

    Authentication with gmail account

---
+++ googleLogin2/app/Http/Controllers/GoogleController.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\Http\Controllers;  
+use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
+use Exception;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use App\view;
+
+
+class GoogleController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+    public function handleGoogleCallback()
+    {
+        try {
+      
+            $user = Socialite::driver('google')->user();
+            
+       
+            $finduser = User::where('google_id', $user->id)->first();
+       
+            if($finduser){
+       
+                Auth::login($finduser);
+                
+      
+                //return redirect()->intended('dashboard');
+                return redirect()->intended('welcome');//added by yasir
+       
+            }else{
+                return view('hina');
+                echo "inserting user detail";
+                $newUser = User::create([
+                    'name' => $user->name,
+                    'email' => $user->email,
+                    'google_id'=> $user->id,
+                    'password' => encrypt('123456dummy')
+                ]);
+      
+                Auth::login($newUser);
+      
+                return redirect()->intended('dashboard');
+            }
+      
+        } catch (Exception $e) {
+            dd($e->getMessage());
+        }
+    }
+    public function logout()
+    {
+        $user = Auth::user();
+        if (Auth::check())
+        {
+            Auth::logout();
+            //return redirect()->intended('yasir');//added by yasir
+            return view('yasir');
+        }
+        else
+        {
+            return view('hina');
+
+        }
+        
+    }
+}
---
+++ googleLogin2/resources/views/hina.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>My jan Hina</title>
+</head>
+<body>
+
+<h1>Already logout Dude</h1>
+
+
+</body>
+</html>
